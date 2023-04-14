2fee7169cc1fac87ff4813e11d6090c0fe843a37
Juro Oravec <juraj.oravec.josefson@gmail.com>
2023-04-14 15:48:22 +0200

    eslint: disable explicit-module-boundary-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,11 @@
 module.exports = {
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     parser: '@typescript-eslint/parser',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/no-var-requires': 'off',
-  }
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
 };
